fb72f421f17e4c90b861db483867b85471cee1b5
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-26 13:47:57 -0300

    3 - Fiz um programa que retorna o maior de tres numeros

--- bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
@@ -4,22 +4,35 @@
 
 // let adicao = a + b;
 // let subtracao = a - b;
-// let multiplicacao = a* b;
+// let multiplicacao = a * b;
 // let divisao = a / b;
 // let modulo = a % b;
 
-// console.log( 'O resultado da adição de ' + a + ' mais ' + b + ' é igual à ' + adicao + '.');
-// console.log( 'O resultado da subtração de ' + a + ' menos ' + b + ' é igual à ' + subtracao + '.');
-// console.log( 'O resultado da multiplicação de ' + a + ' vezes ' + b + ' é igual à ' + multiplicacao + '.');
-// console.log( 'O resultado da divisao de ' + a + ' por ' + b + ' é igual à ' + divisao + '.');
-// console.log( 'O resultado do módulo de ' + a + ' por ' + b + ' é igual à ' + modulo + '.');
+// console.log('O resultado da adição de ' + a + ' mais ' + b + ' é igual à ' + adicao + '.');
+// console.log('O resultado da subtração de ' + a + ' menos ' + b + ' é igual à ' + subtracao + '.');
+// console.log('O resultado da multiplicação de ' + a + ' vezes ' + b + ' é igual à ' + multiplicacao + '.');
+// console.log('O resultado da divisao de ' + a + ' por ' + b + ' é igual à ' + divisao + '.');
+// console.log('O resultado do módulo de ' + a + ' por ' + b + ' é igual à ' + modulo + '.');
 
 // Programa 02
-let a = 18;
-let b = 125;
+// let a = 18;
+// let b = 125;
 
-if(a > b){
-    console.log('O número ' + a + ' é maior que o número ' + b);
-} else{
-    console.log('O número ' + b + ' é maior que o número ' + a)
+// if (a > b) {
+//     console.log('O número ' + a + ' é maior que o número ' + b + '.');
+// } else {
+//     console.log('O número ' + b + ' é maior que o número ' + a + '.')
+// }
+
+//Programa 03
+let a = 34;
+let b = 98;
+let c = 59;
+
+if (a > b && a > c) {
+    console.log('O número ' + a + ' é maior que os números ' + b + ' e ' + c + '.');
+} else if (b > a && b > c) {
+    console.log('O número ' + b + ' é maior que os números ' + a + ' e ' + c + '.');
+} else {
+    console.log('O número ' + c + ' é maior que os números ' + a + ' e ' + b + '.');
 }
