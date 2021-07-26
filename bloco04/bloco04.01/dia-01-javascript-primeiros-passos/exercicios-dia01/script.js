a2023608faeed096490efab2a42f4cfa2e220f9f
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-26 13:39:26 -0300

    2 - Fiz um programa que retorna o maior de doi numeros

--- bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
@@ -1,16 +1,25 @@
 // Programa 01
+// let a = 13;
+// let b = 9;
 
-let a = 13;
-let b = 9;
+// let adicao = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a* b;
+// let divisao = a / b;
+// let modulo = a % b;
 
-let adicao = a + b;
-let subtracao = a - b;
-let multiplicacao = a* b;
-let divisao = a / b;
-let modulo = a % b;
+// console.log( 'O resultado da adição de ' + a + ' mais ' + b + ' é igual à ' + adicao + '.');
+// console.log( 'O resultado da subtração de ' + a + ' menos ' + b + ' é igual à ' + subtracao + '.');
+// console.log( 'O resultado da multiplicação de ' + a + ' vezes ' + b + ' é igual à ' + multiplicacao + '.');
+// console.log( 'O resultado da divisao de ' + a + ' por ' + b + ' é igual à ' + divisao + '.');
+// console.log( 'O resultado do módulo de ' + a + ' por ' + b + ' é igual à ' + modulo + '.');
 
-console.log( 'O resultado da adição de ' + a + ' mais ' + b + ' é igual à ' + adicao + '.');
-console.log( 'O resultado da subtração de ' + a + ' menos ' + b + ' é igual à ' + subtracao + '.');
-console.log( 'O resultado da multiplicação de ' + a + ' vezes ' + b + ' é igual à ' + multiplicacao + '.');
-console.log( 'O resultado da divisao de ' + a + ' por ' + b + ' é igual à ' + divisao + '.');
-console.log( 'O resultado do módulo de ' + a + ' por ' + b + ' é igual à ' + modulo + '.');
+// Programa 02
+let a = 18;
+let b = 125;
+
+if(a > b){
+    console.log('O número ' + a + ' é maior que o número ' + b);
+} else{
+    console.log('O número ' + b + ' é maior que o número ' + a)
+}
