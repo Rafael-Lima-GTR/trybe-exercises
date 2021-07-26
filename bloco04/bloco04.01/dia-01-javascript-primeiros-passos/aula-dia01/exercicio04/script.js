755923d7451e36daa77e6f068e95acabe2f2fd46
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-26 09:13:41 -0300

    2 - Criei uma estrutura condicional com o switch/case que imprimiu as mensagens "aprovada", "reprovada" ou reprovada; ainda foi criado um default, que imprime a mensagem "nao se aplica"

--- bloco04/bloco04.01/dia-01-javascript-primeiros-passos/aula-dia01/exercicio04/script.js
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/aula-dia01/exercicio04/script.js
@@ -1,3 +1,22 @@
 let estadoCandidato = 'lista';
-estadoCandidato = 'aprovada';
-estadoCandidato = 'reprovada';
+// estadoCandidato = 'aprovada';
+// estadoCandidato = 'reprovada';
+
+switch(estadoCandidato){
+
+    case 'aprovada':
+        console.log("Parabéns, você foi aprovada(o)!!!");
+        break;
+
+    case 'lista':
+        console.log("Você está na nossa lista de espera.");
+        break;
+
+    case 'reprovada':
+        console.log("Você foi reprovada(o)!");
+        break;
+
+    default:
+        console.log("Não se aplica.");
+        break;
+}
