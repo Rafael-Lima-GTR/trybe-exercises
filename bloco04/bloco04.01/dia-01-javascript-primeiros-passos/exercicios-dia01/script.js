2a71feedb0e7503922c138667cb682a47b1e4fb9
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-26 13:32:13 -0300

    1 - Foi criado um programa para cada operaçao aritmetica basica.

---
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
@@ -0,0 +1,16 @@
+// Programa 01
+
+let a = 13;
+let b = 9;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a* b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log( 'O resultado da adição de ' + a + ' mais ' + b + ' é igual à ' + adicao + '.');
+console.log( 'O resultado da subtração de ' + a + ' menos ' + b + ' é igual à ' + subtracao + '.');
+console.log( 'O resultado da multiplicação de ' + a + ' vezes ' + b + ' é igual à ' + multiplicacao + '.');
+console.log( 'O resultado da divisao de ' + a + ' por ' + b + ' é igual à ' + divisao + '.');
+console.log( 'O resultado do módulo de ' + a + ' por ' + b + ' é igual à ' + modulo + '.');
