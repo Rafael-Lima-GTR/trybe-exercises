f853289c6e3c13598db3fa4c84000c87ae835c59
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-26 17:51:25 -0300

    6 - Criei um programa que recebe o nome de uma peça de xadrez e retorna os movimentos que ela faz

--- bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
@@ -30,7 +30,7 @@
 //     console.log('O número ' + b + ' é maior que o número ' + a + '.')
 // }
 
-//Programa 03
+// Programa 03
 // let a = 34;
 // let b = 98;
 // let c = 59;
@@ -43,7 +43,7 @@
 //     console.log('O número ' + c + ' é maior que os números ' + a + ' e ' + b + '.');
 // }
 
-// //Programa 04
+// Programa 04
 // let numTestado = 33;
 
 // if (Math.sign(numTestado) > 0) {
@@ -56,20 +56,57 @@
 //     console.log('O valor ' + numTestado + " não é um número válido.");
 // };
 
-//Programa 05
-let primeiroAngulo = 150;
-let segundoAngulo = 15;
-let terceiroAngulo = 30;
+// Programa 05
+// let primeiroAngulo = 150;
+// let segundoAngulo = 15;
+// let terceiroAngulo = 30;
 
-let valorTotalDosAngulos = primeiroAngulo + segundoAngulo + terceiroAngulo;
+// let valorTotalDosAngulos = primeiroAngulo + segundoAngulo + terceiroAngulo;
 
-if (primeiroAngulo > 0 && segundoAngulo > 0 && terceiroAngulo > 0) {
+// if (primeiroAngulo > 0 && segundoAngulo > 0 && terceiroAngulo > 0) {
 
-    if (valorTotalDosAngulos === 180) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-} else {
-    console.log('Erro de ângulo inválido');
+//     if (valorTotalDosAngulos === 180) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// } else {
+//     console.log('Erro de ângulo inválido');
+// }
+
+// Programa 06
+
+let pecaDeXadrez = 'tcasa';
+
+switch (pecaDeXadrez.toLowerCase()) {
+    case 'rei':
+        console.log( 'Rei -> horizontal, vertical e diagonal somente uma casa de cada vez.');
+        break;
+
+    case 'rainha':
+        console.log('Rainha -> ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.');
+        break;
+
+    case 'bispo':
+        console.log('Bispo -> ao longo das diagonais mas não pode pular outras peças.');
+        break;
+
+    case 'cavalo':
+        console.log('Cavalo -> forma de “L”, quer dizer, duas casas em sentido horizontal e ' +
+            'mais uma na vertical ou vice-versa. Única peça que pode pular as outras.');
+        break;
+
+    case 'torre':
+        console.log('Torre -> vertical ou horizontal, mas não pode pular outras peças.');
+        break;
+
+    case 'peão':
+        console.log('Peão -> uma casa para frente e somente captura outras peças na diagonal.' +
+            'Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.'
+        );
+        break;
+    
+    default:
+        console.log('Erro, peça inválida');
+        break;
 }
