c2d1a45f571d796e5741aa59e3731fef12036604
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-25 21:20:09 -0300

    Criado pasta e arquivos iniciais do exercicio usado no exemplo

---
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/aula-dia01/exercicio02/script.js
@@ -0,0 +1,16 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patienteId);
+console.log(typeof isEnrolled);
+console.log(typeof patienteInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+patienteId = '50';
+console.log(typeof patienteId);
