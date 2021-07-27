9d18da2ac8d3cac9bca2bd124f05103934f36adb
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-26 23:13:53 -0300

    11 - Fiz um programa que, dado um salário bruto, calculei o líquido a ser recebido.

--- bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
+++ bloco04/bloco04.01/dia-01-javascript-primeiros-passos/exercicios-dia01/script.js
@@ -165,13 +165,36 @@
 //     console.log('O lucro  que a empresa terá ao vender mil desses produtos será de R$' + lucro);
 // }
 
-let costOfProduct = 10;
-let saleValue = 30;
+// Programa 11
 
-if (costOfProduct >= 0 && saleValue >= 0) {
-  let totalCostOfProduct = costOfProduct * 1.2;
-  let totalProfit = (saleValue - totalCostOfProduct) * 1000;
-  console.log(totalProfit);
+let aliquotaINSS;
+let aliquotaIR;
+
+let salarioBruto = 800
+
+if (salarioBruto <= 1556.94){
+    aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92){
+    aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * 0.11;
 } else {
-  console.log("Error, os valores não podem ser negativos");
-};
+    aliquotaINSS = 570.88;
+}
+
+let salarioBase = salarioBruto - aliquotaINSS;
+
+if (salarioBase <= 1903.98){
+    aliquotaIR = 0;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65){
+    aliquotaIR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05){
+    aliquotaIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68){
+    aliquotaIR = (salarioBase * 0.225) - 636.13;
+} else{
+    aliquotaIR = (salarioBase * 0.275) - 869.36;
+}
+
+let salarioLiquido = salarioBase - aliquotaIR;
+console.log('Salário é R$' + salarioLiquido);
