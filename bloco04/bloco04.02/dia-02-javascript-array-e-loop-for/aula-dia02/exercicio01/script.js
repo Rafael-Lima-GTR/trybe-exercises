7a647ac8d242a31030b4d9701fbb177264c9f789
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-27 13:25:47 -0300

    1 - Fiz os primeiros exercicios citados dos exemplos Arrays(listas)

---
+++ bloco04/bloco04.02/dia-02-javascript-array-e-loop-for/aula-dia02/exercicio01/script.js
@@ -0,0 +1,52 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa no final do array
+// console.log(tasksList);
+// // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// tasksList.unshift('Tocar guitarra'); // adiciona mais uma tarefa no inicio do array
+// console.log(tasksList);
+// // ['Tocar guitarra','Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+// // ['Tocar guitarra','Tomar café', 'Reunião', 'Brincar com o cachorro']
+
+// tasksList.shift();  // remove a primeira tarefa
+// console.log(tasksList);
+// // ['Tomar café', 'Reunião', 'Brincar com o cachorro']
+
+// let indexOfTask = tasksList.indexOf('Reunião'); //indexOf() usado para procurar o índice de um item no Array 
+// console.log(indexOfTask);
+// // 1
+
+// // Programa 01 - Obtenha o valor "Serviços" do array menu:
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// // Programa 02 - Procure o índice do valor "Portfólio" do array menu:
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// Programa 03 - Adicione o valor "Contato" no final do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
+
+
