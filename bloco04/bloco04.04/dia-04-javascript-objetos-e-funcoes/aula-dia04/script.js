609b96108d080eaf9fc697c76be16508a5650418
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 19:14:36 -0300

    2 - Criado o console.log do exercicio

--- bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/aula-dia04/script.js
+++ bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/aula-dia04/script.js
@@ -4,9 +4,16 @@
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
 
-let player ={
+let player = {
     name: 'Marta',
-    lastName = 'Silva',
-    age = 34,
-    medals = {golden: 2, silver: 3}
+    lastName: 'Silva',
+    age: 34,
+    medals: {golden: 2, silver: 3}
 };
+
+// 2 - Acesse as chaves name , lastName e age e concatene as suas 
+// informações para imprimir no console uma mensagem no seguinte 
+// formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName +
+' tem ' + player.age + ' anos de idade');
