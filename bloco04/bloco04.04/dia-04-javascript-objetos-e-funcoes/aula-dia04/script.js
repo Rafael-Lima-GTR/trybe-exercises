df4a27b770436761f5bf473e4745cd030bb07c7c
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 20:57:42 -0300

    1 - Feito o primeiro exercicio para fixar do For/In

--- bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/aula-dia04/script.js
+++ bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/aula-dia04/script.js
@@ -29,7 +29,51 @@ let player = {
 // console.log('A jogadora ' + player.name + ' ' + player.lastName + 
 // ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
 
-// 5 - Acesse a chave medals e faça um console.log no seguinte formato:
-// "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' +
-player.medals.silver + ' medalhas de prata');
+// // 5 - Acesse a chave medals e faça um console.log no seguinte formato:
+// // "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' +
+// player.medals.silver + ' medalhas de prata');
+
+//Exemplos For/In
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+
+// for (let position in food) {
+//     console.log(position);
+//   };
+//   //resultado: 0, 1, 2;
+
+
+// for (let value of food) {
+//   console.log(value);
+// };
+//resultado: hamburguer, bife, acarajé;
+
+
+// Para fixar
+// 1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx'
+//  para cada nome, substituindo o xxxxx pelo nome em questão.
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+for(let key in names){
+    console.log('Olá ' + names[key]);
+}
