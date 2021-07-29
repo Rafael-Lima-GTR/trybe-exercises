ea623126291bbad246c0bf8ef4c7f3fe5f1841b3
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 15:03:38 -0300

    1 - Foi solucionado o exercicio de fixaçao do Bloco 4.3 citado no inicio

---
+++ bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
@@ -0,0 +1,15 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, 
+// dado um valor n qualquer, seja n > 1 , imprima na tela um 
+// quadrado feito de asteriscos de lado de tamanho n.
+
+let n = 7;
+let oneLine = ''
+
+for (let index = 0; index < n; index += 1) {
+
+    oneLine = oneLine + '*';
+}
+
+for (let index = 0; index < n; index += 1) {
+    console.log(oneLine);
+}
