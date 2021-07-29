35211d4c5eff80ecdef17be83b6e7c0516e0b8ba
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 16:52:28 -0300

    5 - Fix um debbuger do exercicio 5 para compreender melhor a logica do que foi feito no gabarito

--- bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
+++ bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
@@ -28,20 +28,64 @@
 // }
 
 
-// 3- Agora inverta o lado do triângulo. (Resolução desse exercício, foi necessário eu consultar a resolução no gabarito do Bloco)
-let n = 5;
-let oneLine = '';
-let inputPosition = n;
+// // 3- Agora inverta o lado do triângulo. (Resolução desse exercício, foi necessário eu consultar a resolução no gabarito do Bloco)
+// let n = 5;
+// let oneLine = '';
+// let inputPosition = n;
 
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex < inputPosition) {
-      oneLine = oneLine + ' ';
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex < inputPosition) {
+//       oneLine = oneLine + ' ';
+//     } else {
+//       oneLine = oneLine + '*';
+//     }
+//   }
+//   console.log(oneLine);
+//   oneLine = '';
+//   inputPosition -= 1;
+// };
+
+
+// // 4- Depois, faça uma pirâmide com n asteriscos de base:
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// let midOfMatrix = (n + 1) / 2;
+// let controlLeft = midOfMatrix;
+// let controlRight = midOfMatrix;
+
+// for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex > controlLeft && columnIndex < controlRight) {
+//       inputLine = inputLine + symbol;
+//     } else {
+//       inputLine = inputLine + ' ';
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   controlRight += 1;
+//   controlLeft -= 1
+// };
+
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
     } else {
-      oneLine = oneLine + '*';
+      outputLine += ' ';
     }
   }
-  console.log(oneLine);
-  oneLine = '';
-  inputPosition -= 1;
-};
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
