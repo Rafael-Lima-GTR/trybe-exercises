90d7733296c34e770465e3ed787315bee3774131
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 15:10:55 -0300

    2 - Foi solucionado o exercicio de fixaçao do Bloco 4.3 citado no inicio

--- bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
+++ bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
@@ -1,15 +1,28 @@
-// 1- Para o primeiro exercício de hoje, faça um programa que, 
-// dado um valor n qualquer, seja n > 1 , imprima na tela um 
-// quadrado feito de asteriscos de lado de tamanho n.
+// // 1- Para o primeiro exercício de hoje, faça um programa que, 
+// // dado um valor n qualquer, seja n > 1 , imprima na tela um 
+// // quadrado feito de asteriscos de lado de tamanho n.
 
-let n = 7;
+// let n = 7;
+// let oneLine = ''
+
+// for (let index = 0; index < n; index += 1) {
+
+//     oneLine = oneLine + '*';
+// }
+
+// for (let index = 0; index < n; index += 1) {
+//     console.log(oneLine);
+// }
+
+// 2- Para o segundo exercício, faça o mesmo que antes,
+// mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
 let oneLine = ''
 
 for (let index = 0; index < n; index += 1) {
 
     oneLine = oneLine + '*';
-}
-
-for (let index = 0; index < n; index += 1) {
     console.log(oneLine);
 }
+
