9a5f44cebda534cf00c01b1c69d06ebbc4e8eee0
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 16:11:26 -0300

    3 - Foi solucionado o exercicio de fixaçao do Bloco 4.3 citado no inicio, foi necessario consultar o gabarito

--- bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
+++ bloco04/bloco04.03/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-dia03/script.js
@@ -14,15 +14,34 @@
 //     console.log(oneLine);
 // }
 
-// 2- Para o segundo exercício, faça o mesmo que antes,
-// mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
+// // 2- Para o segundo exercício, faça o mesmo que antes,
+// // mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+// let n = 5;
+// let oneLine = ''
+
+// for (let index = 0; index < n; index += 1) {
+
+//     oneLine = oneLine + '*';
+//     console.log(oneLine);
+// }
+
+
+// 3- Agora inverta o lado do triângulo. (Resolução desse exercício, foi necessário eu consultar a resolução no gabarito do Bloco)
 let n = 5;
-let oneLine = ''
+let oneLine = '';
+let inputPosition = n;
 
-for (let index = 0; index < n; index += 1) {
-
-    oneLine = oneLine + '*';
-    console.log(oneLine);
-}
-
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      oneLine = oneLine + ' ';
+    } else {
+      oneLine = oneLine + '*';
+    }
+  }
+  console.log(oneLine);
+  oneLine = '';
+  inputPosition -= 1;
+};
