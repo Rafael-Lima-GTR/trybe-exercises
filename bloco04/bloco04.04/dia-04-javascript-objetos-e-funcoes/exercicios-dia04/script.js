b18323ecef5743f17a11462ef927a580a2f7cb0e
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-30 13:07:31 -0300

    5 - Foi criado um segundo objeto e logo apos impresso conforme proposto no exercicio

--- bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/exercicios-dia04/script.js
+++ bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/exercicios-dia04/script.js
@@ -19,7 +19,25 @@ info['recorrente'] = 'Sim';
 //     console.log(key);
 // }
 
-// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+// // 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+// for (let key in info) {
+//     console.log(info[key]);
+// }
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro 
+//e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178',
+// 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
 for (let key in info) {
-    console.log(info[key]);
+    if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key] + ' e ' + info2[key]);
+    }
 }
