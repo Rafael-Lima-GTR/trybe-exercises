e782cbb653646c75e9d9221915241f5eaf7b8312
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-29 21:01:54 -0300

    2 - Feito o segundo exercicio para fixar do For/In

--- bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/aula-dia04/script.js
+++ bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/aula-dia04/script.js
@@ -8,7 +8,7 @@ let player = {
     name: 'Marta',
     lastName: 'Silva',
     age: 34,
-    medals: {golden: 2, silver: 3}
+    medals: { golden: 2, silver: 3 }
 };
 
 // // 2 - Acesse as chaves name , lastName e age e concatene as suas 
@@ -46,7 +46,7 @@ let player = {
 //     model: '500',
 //     color: 'white',
 //   };
-  
+
 //   for (let index in car) {
 //     console.log(index, car[index]);
 //   }
@@ -65,15 +65,27 @@ let player = {
 //resultado: hamburguer, bife, acarajé;
 
 
-// Para fixar
-// 1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx'
-//  para cada nome, substituindo o xxxxx pelo nome em questão.
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge' 
-  };
+// // Para fixar
+// // 1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx'
+// //  para cada nome, substituindo o xxxxx pelo nome em questão.
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge'
+// };
 
-for(let key in names){
-    console.log('Olá ' + names[key]);
+// for (let key in names) {
+//     console.log('Olá ' + names[key]);
+// }
+
+// 2 - Usando o objeto abaixo, utilize For/in e imprima um console.log 
+// com as chaves e valores desse objeto.
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (let key in car) {
+    console.log(key, car[key]);
 }
