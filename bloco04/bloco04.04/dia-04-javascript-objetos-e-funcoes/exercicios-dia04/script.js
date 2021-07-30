b973f23c771a8b8e77d30b976a028447a2714a09
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-30 13:47:48 -0300

    1 - Criada funçao que recebe string e verifica se e um palidromo

--- bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/exercicios-dia04/script.js
+++ bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/exercicios-dia04/script.js
@@ -24,20 +24,36 @@ info['recorrente'] = 'Sim';
 //     console.log(info[key]);
 // }
 
-// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro 
-//e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178',
-// 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-};
+// // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro 
+// //e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178',
+// // 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+// };
 
-for (let key in info) {
-    if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
-        console.log('Ambos recorrentes');
+// for (let key in info) {
+//     if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+//         console.log('Ambos recorrentes');
+//     } else {
+//         console.log(info[key] + ' e ' + info2[key]);
+//     }
+// }
+
+// Parte II - Funções
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+let stringWord = 'arara'
+
+function palindromoTest(stringWord) {
+    const reverseString = stringWord.split('').reverse().join('');
+    if (reverseString === stringWord) {
+        return true;
     } else {
-        console.log(info[key] + ' e ' + info2[key]);
+        return false;
     }
 }
+
+console.log(palindromoTest(stringWord));
+console.log(palindromoTest('desenvolvimento'));
