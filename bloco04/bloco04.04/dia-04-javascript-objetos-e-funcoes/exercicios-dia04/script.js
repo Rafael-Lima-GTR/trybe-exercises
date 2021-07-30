6ae2b66fa1282898db19fa579067116f00dd47db
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-30 17:39:10 -0300

    5 - Foi necessario eu consultar o gabarito do bloco 4.4 e copiar o codigo pra entender a logic aque foi usada para identificar o elemento com o maior numero de vezes repetido

--- bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/exercicios-dia04/script.js
+++ bloco04/bloco04.04/dia-04-javascript-objetos-e-funcoes/exercicios-dia04/script.js
@@ -62,7 +62,7 @@ info['recorrente'] = 'Sim';
 // let arrayTeste = [2, 3, 6, 7, 10, 1];
 
 // function maiorNumero(arrayTeste){
-    
+
 //     let higherNumber = 0;
 //     for (let index in arrayTeste){
 //         if (arrayTeste[higherNumber] < arrayTeste[index]){
@@ -81,7 +81,7 @@ info['recorrente'] = 'Sim';
 // let arrayTeste = [2, 4, 6, 7, 10, 0, -3];
 
 // function menorNumero(arrayTeste){
-    
+
 //     let smallerNumber = 0;
 //     for (let index in arrayTeste){
 //         if (arrayTeste[smallerNumber] > arrayTeste[index]){
@@ -94,19 +94,45 @@ info['recorrente'] = 'Sim';
 
 // console.log(menorNumero(arrayTeste));
 
-// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
-let arrayTeste = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// // 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// let arrayTeste = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-function maiorCaracter(arrayTeste){
+// function maiorCaracter(arrayTeste){
+
+//     let higherCaracterNumber = 0;
+//     for (let index in arrayTeste){
+//         if (arrayTeste[higherCaracterNumber].length < arrayTeste[index].length){
+//              higherCaracterNumber = index;    
+//         }    
+//     }
+
+//     return arrayTeste[higherCaracterNumber];
+// }
+
+// console.log(maiorCaracter(arrayTeste));
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+let arrayTeste = [2, 3, 2, 5, 8, 2, 3];
+
+function maisRepetido(numeros) {
     
-    let higherCaracterNumber = 0;
-    for (let index in arrayTeste){
-        if (arrayTeste[higherCaracterNumber].length < arrayTeste[index].length){
-             higherCaracterNumber = index;    
-        }    
+    let contRepetido = 0;
+    let contNumero = 0;
+    let indexNumeroRepetido = 0;
+    for (let index in numeros) {
+        let verificaNumero = numeros[index];
+        for (let index2 in numeros) {
+            if (verificaNumero === numeros[index2]) {
+                contNumero += 1;
+            }
+        }
+        if (contNumero > contRepetido) {
+            contRepetido = contNumero;
+            indexNumeroRepetido = index;
+        }
+        contNumero = 0;
     }
-
-    return arrayTeste[higherCaracterNumber];
+    return numeros[indexNumeroRepetido];
 }
 
-console.log(maiorCaracter(arrayTeste));
+console.log(maisRepetido(arrayTeste));
