dd8eb4e921ef4656f646995287f985f254bb3e33
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-31 17:40:28 -0300

    1 - Criei um irmão para `elementoOndeVoceEsta`.

--- bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/aula-dia02/script.js
+++ bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/aula-dia02/script.js
@@ -1,27 +1,43 @@
-// Parte I - Buscando elementos
+// // Parte I - Buscando elementos
 
-// 1 - Acesse o elemento elementoOndeVoceEsta.
-const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// // 1 - Acesse o elemento elementoOndeVoceEsta.
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
-// 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-const father = elementoOndeVoceEsta.parentElement;
-    father.style.color = 'orange';
+// // 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// const father = elementoOndeVoceEsta.parentElement;
+//     father.style.color = 'orange';
 
-// 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-const firstSonOfSon = elementoOndeVoceEsta.firstElementChild;
-    firstSonOfSon.innerText = 'primeiroFilhoDoFilho';
+// // 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// const firstSonOfSon = elementoOndeVoceEsta.firstElementChild;
+//     firstSonOfSon.innerText = 'primeiroFilhoDoFilho';
 
-// 4 - Acesse o primeiroFilho a partir de pai.
-let firstSon = father.firstElementChild;
+// // 4 - Acesse o primeiroFilho a partir de pai.
+// let firstSon = father.firstElementChild;
 
-// 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
-firstSon = elementoOndeVoceEsta.previousElementSibling;
+// // 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// firstSon = elementoOndeVoceEsta.previousElementSibling;
 
-// 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
-const textAcess = elementoOndeVoceEsta.nextSibling;
+// // 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// const textAcess = elementoOndeVoceEsta.nextSibling;
 
-// 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
-let thirdSon = elementoOndeVoceEsta.nextElementSibling;
+// // 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// let thirdSon = elementoOndeVoceEsta.nextElementSibling;
 
-// 8 - Agora acesse o terceiroFilho a partir de pai.
-thirdSon = father.children[2];
+// // 8 - Agora acesse o terceiroFilho a partir de pai.
+// thirdSon = father.children[2];
+
+
+// Parte II - Criando elementos
+// 1. Crie um irmão para `elementoOndeVoceEsta`.
+const father = document.getElementById('pai');
+const brotherElementoOndeVoceEsta = document.createElement('section');
+brotherElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+father.appendChild(irmaoElementoOndeVoceEsta); 
+
+// 2. Crie um filho para `elementoOndeVoceEsta`.
+
+
+// 3. Crie um filho para `primeiroFilhoDoFilho`.
+
+
+// 4. A partir desse filho criado, acesse `terceiroFilho`.
