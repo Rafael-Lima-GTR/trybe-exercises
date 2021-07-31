d444c468b176e1b15169d22bcdf5f6203b0be43b
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-31 14:24:37 -0300

    5 e 6 - Foram criadas as funçoes de acordo com o proposto no exercicio

--- bloco05/bloco05.01/dia-01-javascript-dom-e-seletores/exercicios-dia01/script.js
+++ bloco05/bloco05.01/dia-01-javascript-dom-e-seletores/exercicios-dia01/script.js
@@ -5,21 +5,47 @@
 
 // 1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos.
 // (Não gaste tempo pensando no texto e sim realizando o exercício)
-const myText = document.getElementsByTagName('p')[1];
-myText.innerText = 'Já me vejo trabalhando na área e muito bem realizado tanto no' +
-' âmbito profissional, como no pessoal!';
+function changeText() {
+    const myText = document.getElementsByTagName('p')[1];
+    myText.innerText = 'Já me vejo trabalhando na área e muito bem realizado tanto no' +
+        ' âmbito profissional, como no pessoal!';
+}
+changeText();
 
 // 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
-const mainContent = document.getElementsByClassName('main-content')[0];
-mainContent.style.backgroundColor = 'rgb(76,164,109)';
+function changeSquareToGreen() {
+    const mainContent = document.getElementsByClassName('main-content')[0];
+    mainContent.style.backgroundColor = 'rgb(76,164,109)';
+}
+changeSquareToGreen();
 
 // 3. Crie uma função que mude a cor do quadrado vermelho para branco.
-const centerContent = document.getElementsByClassName('center-content')[0];
-centerContent.style.backgroundColor = 'rgb(255,255,255)';
+function changeSquareToWhite() {
+    const centerContent = document.getElementsByClassName('center-content')[0];
+    centerContent.style.backgroundColor = 'rgb(255,255,255)';
+}
+changeSquareToWhite();
 
 // 4. Crie uma função que corrija o texto da tag <h1>.
-const textCorrection = document.getElementsByClassName('title')[0];
-textCorrection.innerText = 'Exercício 5.1 - JavaScript';
+function correctTitle() {
+    const textCorrection = document.getElementsByClassName('title')[0];
+    textCorrection.innerText = 'Exercício 5.1 - JavaScript';
+}
+correctTitle();
 
 // 5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+function paragraphUpperCase() {
+    const paragraphUpperCase = document.getElementsByTagName('p')[0];
+    paragraphUpperCase.innerHTML = paragraphUpperCase.innerHTML.toUpperCase();
+}
+paragraphUpperCase();
+
 // 6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+function showParagraphs() {
+    let paragraphs = document.getElementsByTagName('p');
+    
+    for (let index = 0; index < paragraphs.length; index += 1) {
+        console.log(paragraphs[index].innerHTML);
+    }
+}
+showParagraphs();
