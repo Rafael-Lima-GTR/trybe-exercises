86ff77b14c399be2cfd32684817fdc2b54e7139b
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-31 23:45:33 -0300

    6 - Adicionei a tag section com a classe right-content como filho da tag main

--- bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
+++ bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
@@ -24,6 +24,10 @@ sectionLeftCreateElement.className = 'left-content';
 mainCreateElement.appendChild(sectionLeftCreateElement);
 
 // 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+const sectionRightCreateElement = document.createElement('section');
+sectionRightCreateElement.className = 'right-content';
+mainCreateElement.appendChild(sectionRightCreateElement);
+
 // 7 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
 // 8 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // 9 - Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
