f7cb3ad704a6063aeb0d4ab2c0d0e45a6aa0ad20
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-08-01 01:05:45 -0300

    3 - Removi a "sectionLeftCreateElement"

--- bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
+++ bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
@@ -61,17 +61,20 @@ const numberWords = ['Um', 'Dois', 'Três', 'Quatro',
  }
 
 
-// 1.Adicione a classe title na tag h1 criada;
+// 1 - Adicione a classe title na tag h1 criada;
 h1CreateElement.className = 'title';
 
-// 2.Adicione a classe description nas 3 tags h3 criadas;
+// 2 - Adicione a classe description nas 3 tags h3 criadas;
 for(index = 0; index < 3; index += 1){
 
     const h3Description = document.getElementsByTagName('h3')[index];
     h3Description.className = 'description';
  }
 
-// 3. Remova a `section` criado no passo 5 (aquele que possui a classe `left-content`). Utilize a função `.removeChild()`;
+// 3 - Remova a `section` criado no passo 5 (aquele que possui a classe `left-content`).
+// Utilize a função `.removeChild()`;
+mainCreateElement.removeChild(sectionLeftCreateElement);
+
 // 4. Centralize a `section` criado no passo 6 (aquele que possui a classe `right-content`). Dica: para centralizar, basta configurar o `margin-right: auto` da `section`;
 // 5. Troque a cor de fundo do elemento pai da `section` criada no passo 3 (aquela que possui a classe `center-content`) para a cor verde;
 // 6.Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
