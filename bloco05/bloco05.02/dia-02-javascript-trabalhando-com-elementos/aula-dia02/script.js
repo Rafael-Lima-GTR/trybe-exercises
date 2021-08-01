27be02d12cd62925b44b7b0186cac3e6969a6151
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-31 22:21:08 -0300

    1 - Tive de consultar o gabarito e utilizei o debbug pra poder compreender a logica.

--- bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/aula-dia02/script.js
+++ bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/aula-dia02/script.js
@@ -27,26 +27,43 @@
 // thirdSon = father.children[2];
 
 
-// Parte II - Criando elementos
-// 1. Crie um irmão para `elementoOndeVoceEsta`.
-const father = document.getElementById('pai');
-const brotherElementoOndeVoceEsta = document.createElement('section');
-brotherElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
-father.appendChild(irmaoElementoOndeVoceEsta); 
+// // Parte II - Criando elementos
+// // 1. Crie um irmão para `elementoOndeVoceEsta`.
+// const father = document.getElementById('pai');
+// const brotherElementoOndeVoceEsta = document.createElement('section');
+// brotherElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+// father.appendChild(irmaoElementoOndeVoceEsta); 
 
-// 2. Crie um filho para `elementoOndeVoceEsta`.
-const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
-const sonOfElementoOndeVoceEsta = document.createElement('section');
-sonOfElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
-elementoOndeVoceEsta.appendChild(sonOfElementoOndeVoceEsta);
-
-
-// 3. Crie um filho para `primeiroFilhoDoFilho`.
-const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
-const sonOfPrimeiroFilhoDoFilho = document.createElement('section');
-sonOfPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
-primeiroFilhoDoFilho.appendChild(sonOfPrimeiroFilhoDoFilho);
+// // 2. Crie um filho para `elementoOndeVoceEsta`.
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// const sonOfElementoOndeVoceEsta = document.createElement('section');
+// sonOfElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+// elementoOndeVoceEsta.appendChild(sonOfElementoOndeVoceEsta);
 
 
-// 4. A partir desse filho criado, acesse `terceiroFilho`.
-console.log(sonOfPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling);
+// // 3. Crie um filho para `primeiroFilhoDoFilho`.
+// const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+// const sonOfPrimeiroFilhoDoFilho = document.createElement('section');
+// sonOfPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+// primeiroFilhoDoFilho.appendChild(sonOfPrimeiroFilhoDoFilho);
+
+
+// // 4. A partir desse filho criado, acesse `terceiroFilho`.
+// console.log(sonOfPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling);
+
+
+// Parte III - Removendo elementos
+// Remova todos os elementos filhos de paiDoPai exceto pai,
+//  elementoOndeVoceEsta e primeiroFilhoDoFilho.
+const pai = document.getElementById('pai');
+
+for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+    const currentChildren = pai.childNodes[index];
+
+    if (currentChildren.id !== 'elementoOndeVoceEsta') {
+        currentChildren.remove();
+    }
+}
+
+const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+segundoEUltimoFilhoDoFilho.remove();
