2d4e2a344329d07cde63d6011f3e0db37ca24aa2
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-08-01 01:09:48 -0300

    4 - Centralizei a "sectionRightCreateElement"

--- bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
+++ bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
@@ -75,7 +75,10 @@ for(index = 0; index < 3; index += 1){
 // Utilize a função `.removeChild()`;
 mainCreateElement.removeChild(sectionLeftCreateElement);
 
-// 4. Centralize a `section` criado no passo 6 (aquele que possui a classe `right-content`). Dica: para centralizar, basta configurar o `margin-right: auto` da `section`;
+// 4. Centralize a `section` criado no passo 6 (aquele que possui a classe `right-content`).
+// Dica: para centralizar, basta configurar o `margin-right: auto` da `section`;
+sectionRightCreateElement.style.marginRight = 'auto';
+
 // 5. Troque a cor de fundo do elemento pai da `section` criada no passo 3 (aquela que possui a classe `center-content`) para a cor verde;
 // 6.Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
     
