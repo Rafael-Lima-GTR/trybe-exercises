02073daa914e41630f31eeb9042784a23775b2d0
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-07-31 23:06:53 -0300

    2 - Adicionei a tag main com a classe main-content como filho da tag body

--- bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
+++ bloco05/bloco05.02/dia-02-javascript-trabalhando-com-elementos/exercicios-dia02/script.js
@@ -1,9 +1,13 @@
 // 1 - Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 const h1CreateElement = document.createElement('h1');
 h1CreateElement.innerText = 'Exercício 5.2 - JavaScript DOM';
-document.body.appendChild(h1CreateElement); 
+document.body.appendChild(h1CreateElement);
 
-// 2 - Adicione a tag main com a classe main-content como filho da tag body ;
+// 2 - Adicione a tag main com a classe main-content como filho da tag body;
+const mainCreateElement = document.createElement('main');
+mainCreateElement.className = 'main-content'
+document.body.appendChild(mainCreateElement);
+
 // 3 - Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 // 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 // 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
