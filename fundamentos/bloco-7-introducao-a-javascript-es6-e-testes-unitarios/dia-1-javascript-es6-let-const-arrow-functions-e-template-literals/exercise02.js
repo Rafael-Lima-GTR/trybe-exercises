51cae0706b12940d66152d1815a37c904e188003
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-09-10 17:37:43 -0300

    Parte 2 - Exercício 2 foi concluído dentro dos requisitos solicitados  e foi acrescentado nos comentários as duas formas da resolução no course.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercise02.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercise02.js
@@ -3,21 +3,21 @@
 // * Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 // * Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator.
 
-const factorial = number => {
-    let result = number;
-    while (number > 2) {
-        result *= --number;
-    }
+// const factorial = number => {
+//     let result = number;
+//     while (number > 2) {
+//         result *= --number;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-console.log(factorial(0));
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(3));
-console.log(factorial(4));
-console.log(factorial(5));
+// console.log(factorial(0));
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(5));
 
 // // Resolução através do for
 // const factorial = number => {
@@ -35,3 +35,43 @@ console.log(factorial(5));
 // // Resolução de forma recursiva
 // const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
 // console.log(factorial(5));
+
+
+// 2. Crie uma função que receba uma frase e retorne qual a maior palavra.
+// * Exemplo:
+// longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+
+const longestWord = phrase => {
+    // Fonte de pesquisa: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
+    let wordArray = phrase.split(' ');
+
+    // Fonte de pesquisa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    return wordArray.sort((a, b) => b.length - a.length)[0];
+
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
+
+// EXEMPLO 1 do gabarito no course da Trybe
+// const longestWord = text => {
+//     let wordArray = text.split(' ');
+//     let maxLength = 0;
+//     let result = '';
+
+//     for (const word of wordArray) {
+//         if (word.length > maxLength) {
+//             maxLength = word.length;
+//             result = word;
+//         }
+//     }
+
+//     return result;
+// }
+
+// console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
+// EXEMPLO 2 do gabarito no course da Trybe
+// const longestWord = phrase => phrase.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0]
+
+// console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
