a51bf850b856486633fc3bb67a5fb76fa957b16d
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-09-10 21:13:46 -0300

    Copiado resolução do exercício 4 pra análise e compreensão da lógica aplicada nele.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercise02.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercise02.js
@@ -42,16 +42,16 @@
 // longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
 
 
-const longestWord = phrase => {
-    // Fonte de pesquisa: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
-    let wordArray = phrase.split(' ');
+// const longestWord = phrase => {
+//     // Fonte de pesquisa: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
+//     let wordArray = phrase.split(' ');
 
-    // Fonte de pesquisa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-    return wordArray.sort((a, b) => b.length - a.length)[0];
+//     // Fonte de pesquisa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+//     return wordArray.sort((a, b) => b.length - a.length)[0];
 
-}
+// }
 
-console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
+// console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
 
 // EXEMPLO 1 do gabarito no course da Trybe
 // const longestWord = text => {
@@ -75,3 +75,50 @@ console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu
 // const longestWord = phrase => phrase.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0]
 
 // console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
+
+
+// 4. Crie um código JavaScript com a seguinte especificação:
+// Não se esqueça de usar template literals
+// * Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar 
+// e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+// Exemplo:
+//     * String determinada: "Tryber x aqui!"
+//     * Parâmetro: "Bebeto"
+//     * Retorno: "Tryber Bebeto aqui!"
+// * Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+// *Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à 
+// string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+// Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+//     * JavaScript;
+//     * HTML; ... #goTrybe".
+
+
+
+const array = ['HTML', 'CSS', 'Javascript', 'Java', 'SQL'];
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    );
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`);
+
+    result = `
+{result}
+
+    #goTrybe
+    `;
+
+    return result;
+}
+
+console.log(buildSkillsPhrase("Lucas"));
