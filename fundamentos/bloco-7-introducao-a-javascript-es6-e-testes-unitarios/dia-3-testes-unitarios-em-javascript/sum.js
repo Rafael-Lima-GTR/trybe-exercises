cf8ecb097107ef82bb51fe9f3d0710bc75e245c1
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-09-13 00:26:37 -0300

    Function Sum -  Indexação corrigida.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/sum.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 
 function sum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error('parameters must be numbers');
-    }
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
 
-    return a + b;
+  return a + b;
 }
 
 assert.strictEqual(typeof sum, 'function', 'Não é uma função!');
@@ -15,9 +15,9 @@ assert.strictEqual(sum(4, 5), 9, 'Quatro mais cinco é igual a nove!');
 assert.strictEqual(sum(0, 0), 0, 'Quatro mais cinco é igual a nove!');
 // 3. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 assert.throws(() => {
-    sum(4, '5');
-  });
+  sum(4, '5');
+});
 // 4. Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
 assert.throws(() => {
-    sum(4, '5');
-  }, /^Error: parameters must be numbers$/);
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);
