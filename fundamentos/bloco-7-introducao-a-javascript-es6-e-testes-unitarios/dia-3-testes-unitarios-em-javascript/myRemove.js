7f603870b7b05b372c7e000e2a95e9487558750a
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-09-13 00:27:16 -0300

    Function myRemove -  Indexação corrigida.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/myRemove.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/myRemove.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 
 function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+        if (item !== arr[index]) {
+            newArr.push(arr[index]);
+        }
     }
-  }
-  return newArr;
+    return newArr;
 }
 
-assert.strictEqual(typeof myRemove, 'function','Não é uma função!');
+assert.strictEqual(typeof myRemove, 'function', 'Não é uma função!');
 // 1. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], 'O elemento 3 é pra ser removido!');
 // 2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
