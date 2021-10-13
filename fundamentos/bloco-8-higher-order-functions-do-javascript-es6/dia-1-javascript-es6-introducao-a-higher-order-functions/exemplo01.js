66b541370d5bf5b7c44ae9e3f19c33490804a77f
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-10-13 19:27:12 -0300

    Criada a HOF doingThings.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exemplo01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exemplo01.js
@@ -3,3 +3,11 @@ const wakeUp = () => { return 'Acordando!!'; }
 const breakFast = () => { return 'Bora tomar café!!'; }
 
 const goToSleep = () => { return 'Partiu dormir!!'; }
+
+const doingThings = (callback) => {
+    console.log(callback());
+};
+
+doingThings(breakFast);
+doingThings(goToSleep);
+doingThings(wakeUp);
