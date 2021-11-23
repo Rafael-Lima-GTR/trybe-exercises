e9eab5c10f8fc6c9758508d175949b37e4b60141
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-11-22 22:35:09 -0300

    Para fixar find 03

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exemploFind.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exemploFind.js
@@ -1,4 +1,4 @@
-//PARA FIXAR 01
+// // PARA FIXAR 01
 
 // const numbers = [19, 21, 30, 3, 45, 22, 15];
 
@@ -21,12 +21,27 @@
 
 // console.log(findDivisibleBy3And5())
 
-//PARA FIXAR 02
-const names = ['João', 'Irene', 'Fernando', 'Maria'];
+// //PARA FIXAR 02
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-const findNameWithFiveLetters = () => {
-  // Adicione seu código aqui:
-  return names.find((name) => name.length === 5);
-}
+// const findNameWithFiveLetters = () => {
+//   // Adicione seu código aqui:
+//   return names.find((name) => name.length === 5);
+// }
 
-console.log(findNameWithFiveLetters());
+// console.log(findNameWithFiveLetters());
+
+
+//PARA FIXAR 03
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    // Adicione seu código aqui
+    return musicas.find((musica) => musica.id === id);
+  }
+  
+  console.log(findMusic('31031685'))
