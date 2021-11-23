e0f0e5d613bcb82bb1dacb0b1c533b6e2ce07da7
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-11-22 22:17:04 -0300

    Para fixar find 02

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exemploFind.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exemploFind.js
@@ -1,3 +1,5 @@
+//PARA FIXAR 01
+
 // const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 // const findDivisibleBy3And5 = (number) => {
@@ -10,11 +12,21 @@
 
 // console.log(divisibleBy3And5);
 
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  // Adiciona seu código aqui
-  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+// const findDivisibleBy3And5 = () => {
+//   // Adiciona seu código aqui
+//   return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+// }
+
+// console.log(findDivisibleBy3And5())
+
+//PARA FIXAR 02
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
 }
 
-console.log(findDivisibleBy3And5())
+console.log(findNameWithFiveLetters());
