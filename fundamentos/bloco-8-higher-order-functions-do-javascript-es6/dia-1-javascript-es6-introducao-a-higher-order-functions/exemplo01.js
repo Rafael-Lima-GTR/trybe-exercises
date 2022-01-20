89cce3dfe04ccc29e3d99e75a37b942f69aef750
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-01-20 14:08:47 -0300

    Salvando alterações do arquivo exemplo01.js

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exemplo01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exemplo01.js
@@ -1,11 +1,23 @@
-const wakeUp = () => { return 'Acordando!!'; }
+// const wakeUp = () => { return 'Acordando!!'; }
 
-const breakFast = () => { return 'Bora tomar café!!'; }
+// const breakFast = () => { return 'Bora tomar café!!'; }
 
-const goToSleep = () => { return 'Partiu dormir!!'; }
+// const goToSleep = () => { return 'Partiu dormir!!'; }
 
-const doingThings = (callback) => console.log(callback());
+// const doingThings = (callback) => console.log(callback());
 
-doingThings(breakFast);
-doingThings(goToSleep);
-doingThings(wakeUp);
+// doingThings(breakFast);
+// doingThings(goToSleep);
+// doingThings(wakeUp);
+
+const repeat = (number, action) => {
+    for (let count = 0; count <= number; count += 1) {
+      action(count);
+    }
+  };
+  
+  repeat(5, (number) => {
+    if (number % 2 === 0) {
+      console.log(number, 'is even');
+    }
+  });
