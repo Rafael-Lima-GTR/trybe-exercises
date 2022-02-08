5bca46061457ed334afa72d3ec8234c3006ca48a
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-02-08 03:03:03 -0300

    Para Fixar 5, 6 e 7 - Resolução Final.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -4,19 +4,33 @@ class App extends React.Component {
   constructor(){
     super()
     this.leftClick = this.leftClick.bind(this);
+    this.state = {
+      numeroDeCliques01:0,
+      numeroDeCliques02:0,
+      numeroDeCliques03:0
+    }
     this.centerClick = this.centerClick.bind(this);
     this.rightClick = this.rightClick.bind(this);
   }
 
   leftClick() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques01: estadoAnterior.numeroDeCliques01 + 1
+    }))
     console.log('Clicou no botão esquerdo!')
   }
   
   centerClick() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques02: estadoAnterior.numeroDeCliques02 + 1
+    }))
     console.log('Clicou no botão central!')
   }
   
   rightClick() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques03: estadoAnterior.numeroDeCliques03 + 1
+    }))
     console.log('Clicou no botão direito!')
   }
 
@@ -24,9 +38,9 @@ class App extends React.Component {
   render (){
     return (
       <>
-        <button onClick={this.leftClick}>Meu botão Esquerdo</button>
-        <button onClick={this.centerClick}>Meu botão Central</button>
-        <button onClick={this.rightClick}>Meu botão Direito</button>
+        <button onClick={this.leftClick}>Meu botão Esquerdo {this.state.numeroDeCliques01}</button>
+        <button onClick={this.centerClick}>Meu botão Central {this.state.numeroDeCliques02}</button>
+        <button onClick={this.rightClick}>Meu botão Direito {this.state.numeroDeCliques03}</button>
       </>
     );
   }
