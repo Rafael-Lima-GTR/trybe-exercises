1f3d4e90515ae50ff1f50eb8c113baf54fcfb48d
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-02-08 02:34:23 -0300

    Para Fixar 3 e 4 - Resolução Final.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,24 +1,32 @@
 import React from "react";
 
-function leftClick() {
-  console.log('Clicou no botão esquerdo!')
-}
-
-function centerClick() {
-  console.log('Clicou no botão central!')
-}
-
-function rightClick() {
-  console.log('Clicou no botão direito!')
-}
-
 class App extends React.Component {
+  constructor(){
+    super()
+    this.leftClick = this.leftClick.bind(this);
+    this.centerClick = this.centerClick.bind(this);
+    this.rightClick = this.rightClick.bind(this);
+  }
+
+  leftClick() {
+    console.log('Clicou no botão esquerdo!')
+  }
+  
+  centerClick() {
+    console.log('Clicou no botão central!')
+  }
+  
+  rightClick() {
+    console.log('Clicou no botão direito!')
+  }
+
+
   render (){
     return (
       <>
-        <button onClick={leftClick}>Meu botão Esquerdo</button>
-        <button onClick={centerClick}>Meu botão Central</button>
-        <button onClick={rightClick}>Meu botão Direito</button>
+        <button onClick={this.leftClick}>Meu botão Esquerdo</button>
+        <button onClick={this.centerClick}>Meu botão Central</button>
+        <button onClick={this.rightClick}>Meu botão Direito</button>
       </>
     );
   }
