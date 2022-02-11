be344692bcb2edcf58361cd32855c2be9a286d9a
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-02-11 19:29:08 -0300

    Para fixar Questões 1 e 2 - Concluídas

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/example01-componentes-controlados/src/Form.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+    };
+  }
+
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <div>
+        <h1>Estados e React - Componentes Controlados</h1>
+        <form className="form">
+
+          <label htmlFor="city">
+            Choose the city:
+            <select id="city" name="city" defaultValue="">
+              <option value="fortaleza">Fortaleza</option>
+              <option value="rioDeJaneiro">Rio de Janeiro</option>
+              <option value="saoPaulo">São Paulo</option>
+              <option value="florianopolis">Florianópolis</option>
+            </select>
+          </label>
+
+          <label htmlFor="fullName">
+            Full Name:
+            <input
+              id="fullName"
+              type="text"
+              name="fullName"
+              value={ name }
+              onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              name="email"
+              type="email"
+            />
+          </label>
+
+          <label>
+            Fale um pouco mais sobre você.
+            <textarea name="resume" />
+          </label>
+
+
+
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
