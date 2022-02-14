a9fd6505176fc4a4e2bae965e70fec1f26f77866
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-02-14 12:01:55 -0300

    Para fixar Questão 6 - Concluída.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/example01-componentes-controlados/src/Form.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/example01-componentes-controlados/src/Form.jsx
@@ -27,71 +27,70 @@ class Form extends Component {
   }
 
   render() {
-    const { name } = this.state;
+    const { city, fullName, email, resume, comparecer } = this.state;
 
     return (
       <div>
         <h1>Estados e React - Componentes Controlados</h1>
         <form className="form">
 
-          <label htmlFor="city">
-            Choose the city:
-            <select id="city" name="city"
-              defaultValue=""
-              value={name}
-              onChange={this.handleChange}>
-              <option value="fortaleza">Fortaleza</option>
-              <option value="rioDeJaneiro">Rio de Janeiro</option>
-              <option value="saoPaulo">São Paulo</option>
-              <option value="florianopolis">Florianópolis</option>
-            </select>
-          </label>
+          <fieldset>
+            <legend>Dados Pessoais</legend>
+            <label htmlFor="city">
+              Choose the city:
+              <select id="city" name="city"
+                defaultValue="" value={city}
+                onChange={this.handleChange}>
+                <option value="fortaleza">Fortaleza</option>
+                <option value="rioDeJaneiro">Rio de Janeiro</option>
+                <option value="saoPaulo">São Paulo</option>
+                <option value="florianopolis">Florianópolis</option>
+              </select>
+            </label>
 
-          <label htmlFor="fullName">
-            Full Name:
-            <input
-              id="fullName"
-              type="text"
-              name="fullName"
-              value={name}
-              onChange={this.handleChange} />
-          </label>
+            <label htmlFor="fullName">
+              Full Name:
+              <input
+                id="fullName" type="text"
+                name="fullName" value={fullName}
+                onChange={this.handleChange} />
+            </label>
 
-          <label htmlFor="email">
-            Email:
-            <input
-              id="email"
-              name="email"
-              type="email"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
+            <label htmlFor="email">
+              Email:
+              <input
+                id="email" name="email"
+                type="email" value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+          </fieldset>
 
-          <label>
-            Fale um pouco mais sobre você.
-            <textarea name="resume"
-              value={name}
-              onChange={this.handleChange} />
-          </label>
+          <fieldset>
+            <legend>Texto e Arquivos</legend>
+
+            <label>
+              Fale um pouco mais sobre você.
+              <textarea name="resume" value={resume}
+                onChange={this.handleChange} />
+            </label>
+
+            <label>
+              Upload file:
+              <input type="file" ref={this.fileInput} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+          </fieldset>
 
           <label>
             Deseja participar do evento?
             <input
-              id="comparecer"
-              name="comparecer"
-              type="checkbox"
-              value={name}
+              id="comparecer" name="comparecer"
+              type="checkbox" value={comparecer}
               onChange={this.handleChange}
             />
           </label>
-
-          <label>
-            Upload file:
-            <input type="file" ref={this.fileInput} />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
 
         </form>
       </div>
